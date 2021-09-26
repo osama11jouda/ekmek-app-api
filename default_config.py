1d7c0d0a0ffb9399ce9d5de0f9589748162cead7
osama jouda <89156210+osama11jouda@users.noreply.github.com>
2021-09-26 07:36:18 +0300

    Update default_config.py

--- default_config.py
+++ default_config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://pytkniirytqjxc:89c62818f4a189a2429b2451b5cbe8b2deb4c9b729f64a1222b556a1e422287c@ec2-52-19-96-181.eu-west-1.compute.amazonaws.com:5432/d9n0em3nsumcvp"# os.environ.get("DATABASE_URL")  # "sqlite:///data.db"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
